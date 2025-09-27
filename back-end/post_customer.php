f60a92a4eeed8c6aa7cb3e6b3bb9c1005ec4852b
florencekn <2380334@edu.vaniercollege.qc.ca>
2025-09-26 23:20:37 -0400

    Created file to control the GPIO

---
+++ back-end/post_customer.php
@@ -0,0 +1,15 @@
+<?php
+## Connecting to SQLite: (https://kb.hosting.com/docs/connect-to-sqlite-using-php)
+# $myPDO = new PDO('sqlite:/home/example/smth.db');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    # i dont know the fields
+    $name = $_POST['name'];
+    $name = $_POST['name'];
+    $name = $_POST['name'];
+}
+# $db = new SQLite
+# $conn = 
+
+
+?>
