ecccacb10c994ae0887eb3cd9968fe4942263c90
Vacheprime <danatmuradov500@gmail.com>
2025-09-29 21:02:57 -0400

    Customer Model: Implemented the insert statement for the customer table.

--- models/customer_model.py
+++ models/customer_model.py
@@ -1,4 +1,6 @@
-
+from .base_model import BaseModel
+from .exceptions.database_insert_exception import DatabaseInsertException
+from contextlib import closing
 
 class Customer(BaseModel):
 	DB_TABLE = "Customers"
@@ -11,5 +13,32 @@ class Customer(BaseModel):
 		self.email = email
 		self.phone_number = phone_number
 		self.rewards_points = 0
-	
-	
+
+
+	@staticmethod
+	def insertCustomer(customer):
+		# Define the insert statement and values
+		sql = """INSERT INTO Customers(first_name, last_name, email, phone_number, rewards_points)
+			VALUES (:first_name, :last_name, :email, :phone_number, :rewards_points);
+		"""
+		sql_values = {
+			"first_name": customer.first_name,
+			"last_name": customer.last_name,
+			"email": customer.email,
+			"phone_number": customer.phone_number,
+			"rewards_points": customer.rewards_points
+		}
+
+		# Get the connection and cursor
+		with BaseModel._connectToDB() as connection, closing(connection.cursor()) as cursor:
+			try:
+				# Execute the query
+				cursor.execute(sql, sql_values)
+
+				# Set the ID returned
+				customer.customer_id = cursor.lastrowid
+
+				# Commit
+				connection.commit()
+			except Exception as e:
+				raise DatabaseInsertException(f"An unexpected error occured while inserting the customer: {e}")
