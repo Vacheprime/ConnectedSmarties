b9a7c9d72abcb9e658e3ac8e13b487d6049425bd
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-11-09 21:35:13 -0500

    added togglepassword visibility function

--- back-end/static/js/login.js
+++ back-end/static/js/login.js
@@ -1,3 +1,18 @@
+function togglePasswordVisibility(inputId) {
+  const passwordInput = document.getElementById(inputId)
+  const toggleIcon = document.getElementById(`${inputId}-toggle-icon`)
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text"
+    toggleIcon.classList.remove("fa-eye")
+    toggleIcon.classList.add("fa-eye-slash")
+  } else {
+    passwordInput.type = "password"
+    toggleIcon.classList.remove("fa-eye-slash")
+    toggleIcon.classList.add("fa-eye")
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("login-form");
 
